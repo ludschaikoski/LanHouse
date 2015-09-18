3525d5726569e66311ac0c9b15aeca7a5084c278
Ludmila Schaikoski Sutir <ludschaikoski@gmail.com>
2015-09-18 15:42:47 -0300

    projeto de eng de software utilizando uma lan house2

---
+++ src/Usuario/Usuario.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Usuario;
+
+/**
+ *
+ * @author Welington
+ */
+public class Usuario {
+
+    private String nome;
+    private Integer id;
+    private String apelido;
+    private char cpf;
+    private char endereco;
+    private String senha;
+
+    public Usuario(String nome, String apelido, String cpf, String telefone, String senha) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public void setCpf(char cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setEndereco(char endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setTelefone(double telefone) {
+        this.telefone = telefone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public char getCpf() {
+        return cpf;
+    }
+
+    public char getEndereco() {
+        return endereco;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public double getTelefone() {
+        return telefone;
+    }
+    private double telefone;
+
+    public Usuario() {
+
+    }
+}
